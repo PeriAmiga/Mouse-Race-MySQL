d912b6c38663829bfbc7c5e8dd1290e66b488b7b
Peri Amiga <periamiga13@gmail.com>
2024-06-14 17:26:11 +0300

    Update dbSettings.js

--- src/db/dbSettings.js
+++ src/db/dbSettings.js
@@ -1,7 +1,7 @@
 // Database Configuration Settings for Mouse Race Game
-const host = 'localhost';
-const user = 'root';
-const password = '123456';
+const host = '';
+const user = '';
+const password = '';
 const database = 'Mouse_Race_LeaderBoard';
 
 module.exports = { host, user, password, database };
